9b7f8788f3130538ccdb9e3f981e0d113917354a
tommy-khc <tomy123abc@gmail.com>
2024-03-04 14:20:56 -0800

    Completed

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -1,12 +1,18 @@
-const http = require('http');
-const today = require('./today');
+const http = require("http");
+const today = require("./today");
 
 const requestListener = function (req, res) {
   res.writeHead(200);
-  res.end(`Hello, World! The date today is ${today.getDate()}`);
-}
+  if (today.getTimeOfDay() === "morning") {
+    res.end(`Good morning, World! The date today is ${today.getDate()}`);
+  } else if (today.getTimeOfDay() === "afternoon") {
+    res.end(`Good afternoon, World! The date today is ${today.getDate()}`);
+  } else {
+    res.end(`Good evening, World! The date today is ${today.getDate()}`);
+  }
+};
 
 const port = 8080;
 const server = http.createServer(requestListener);
-console.log('server listening on port: ' + port);
+console.log("server listening on port: " + port);
 server.listen(port);
--- CD220Labs/http_server/today.js
+++ CD220Labs/http_server/today.js
@@ -1,4 +1,21 @@
 module.exports.getDate = function getDate() {
-    let aestTime = new Date().toLocaleString("en-US", {timeZone: "Australia/Brisbane"});
-    return new Date(aestTime);
-}
+  let aestTime = new Date().toLocaleString("en-US", {
+    timeZone: "Australia/Brisbane",
+  });
+  return new Date(aestTime);
+};
+
+//to find out if now is in morning, afternoo or evening
+module.exports.getTimeOfDay = function getTimeOfDay() {
+  let aestTime = new Date().toLocaleString("en-US", {
+    timeZone: "Australia/Brisbane",
+  });
+  let hour = new Date(aestTime).getHours();
+  if (hour < 12 && hour > 4) {
+    return "morning";
+  } else if (hour < 18) {
+    return "afternoon";
+  } else {
+    return "evening";
+  }
+};
