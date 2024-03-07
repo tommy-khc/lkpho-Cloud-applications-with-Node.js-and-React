d3f157bb2e72aaffa87b8294a39b37e3ed103c75
Your Name <tomy123abc@gmail.com>
2024-03-06 18:29:58 -0800

    completed express server lab

--- CD220Labs/expressjs/expressWithAuthentication.js
+++ CD220Labs/expressjs/expressWithAuthentication.js
@@ -1,97 +1,108 @@
-const express = require('express');
-const jwt = require('jsonwebtoken');
-const session = require('express-session')
+const express = require("express");
+const jwt = require("jsonwebtoken");
+const session = require("express-session");
 
-let users = []
+// Array to store users
+let users = [];
 
-//Function to check if the user exists
-const doesExist = (username)=>{
-  let userswithsamename = users.filter((user)=>{
-    return user.username === username
-  });
-  if(userswithsamename.length > 0){
-    return true;
-  } else {
-    return false;
-  }
-}
+// Function to check if the user exists
+const doesExist = (username) => {
+  const usersWithSameName = users.filter((user) => user.username === username);
+  return usersWithSameName.length > 0;
+};
 
-//Function to check if the user is authenticated
-const authenticatedUser = (username,password)=>{
-  let validusers = users.filter((user)=>{
-    return (user.username === username && user.password === password)
-  });
-  if(validusers.length > 0){
-    return true;
-  } else {
-    return false;
-  }
-}
+// Function to check if the user is authenticated
+const authenticatedUser = (username, password) => {
+  const validUsers = users.filter(
+    (user) => user.username === username && user.password === password
+  );
+  return validUsers.length > 0;
+};
 
 const app = express();
 
 app.use(express.json());
 
-app.use(session({secret:"fingerpint"}))
+app.use(
+  session({
+    secret: "fingerpint",
+  })
+);
 
-app.use("/auth", function auth(req,res,next){
-   if(req.session.authorization) { //get the authorization object stored in the session
-       token = req.session.authorization['accessToken']; //retrieve the token from authorization object
-       jwt.verify(token, "access",(err,user)=>{ //Use JWT to verify token
-           if(!err){
-               req.user = user;
-               next();
-           }
-           else{
-               return res.status(403).json({message: "User not authenticated"})
-           }
-        });
-    } else {
-        return res.status(403).json({message: "User not logged in"})
-    }
+// Middleware to check if the user is authenticated
+app.use("/auth", function auth(req, res, next) {
+  if (req.session.authorization) {
+    // Get the authorization object stored in the session
+    const token = req.session.authorization["accessToken"]; // Retrieve the token from the authorization object
+    jwt.verify(token, "access", (err, user) => {
+      // Use JWT to verify the token
+      if (!err) {
+        req.user = user;
+        next();
+      } else {
+        return res.status(403).json({ message: "User not authenticated" });
+      }
+    });
+  } else {
+    return res.status(403).json({ message: "User not logged in" });
+  }
 });
 
-app.post("/login", (req,res) => {
+// Route to get a message if the user is authenticated
+app.get("/auth/get_message", (req, res) => {
+  return res.status(200).json({
+    message: "Hello, You are an authenticated user. Congratulations!",
+  });
+});
+
+// Route to login a user
+app.post("/login", (req, res) => {
   const username = req.query.username;
   const password = req.query.password;
 
   if (!username || !password) {
-      return res.status(404).json({message: "Error logging in"});
+    return res.status(404).json({ message: "Error logging in" });
   }
 
-  if (authenticatedUser(username,password)) {
-    let accessToken = jwt.sign({
-      data: password
-    }, 'access', { expiresIn: 60 * 60 });
+  if (authenticatedUser(username, password)) {
+    const accessToken = jwt.sign(
+      {
+        data: password,
+      },
+      "access",
+      { expiresIn: 60 * 60 }
+    );
 
     req.session.authorization = {
-      accessToken,username
-  }
-  return res.status(200).send("User successfully logged in");
+      accessToken,
+      username,
+    };
+    return res.status(200).send("User successfully logged in");
   } else {
-    return res.status(208).json({message: "Invalid Login. Check username and password"});
+    return res
+      .status(208)
+      .json({ message: "Invalid Login. Check username and password" });
   }
 });
 
-app.post("/register", (req,res) => {
+// Route to register a user
+app.post("/register", (req, res) => {
   const username = req.query.username;
   const password = req.query.password;
 
   if (username && password) {
-    if (!doesExist(username)) { 
-      users.push({"username":username,"password":password});
-      return res.status(200).json({message: "User successfully registred. Now you can login"});
+    if (!doesExist(username)) {
+      users.push({ username: username, password: password });
+      return res
+        .status(200)
+        .json({ message: "User successfully registred. Now you can login" });
     } else {
-      return res.status(404).json({message: "User already exists!"});    
+      return res.status(404).json({ message: "User already exists!" });
     }
-  } 
-  return res.status(404).json({message: "Unable to register user."});
+  }
+  return res.status(404).json({ message: "Unable to register user." });
 });
 
-app.get("/auth/get_message", (req,res) => {
-  return res.status(200).json({message: "Hello, You are an authenticated user. Congratulations!"});
-})
+const PORT = 5000;
 
-const PORT =5000;
-
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
